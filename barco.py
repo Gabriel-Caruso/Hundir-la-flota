5c5308325af4b32fdc44d38b02b3686e06d0f9c3
Gabriel Caruso <128610501+Gabriel-Caruso@users.noreply.github.com>
2026-04-02 12:13:17 +0200

    docs added: clases.py
    docs deleted: barco.py and tablero.py

--- barco.py
+++
@@ -1,9 +0,0 @@
-class Barco:
-    def __init__(self,
-                 eslora = int(input("Indica la eslora de tu barco: ")),
-                 nombre = str(input("Escribe el nombre de tu barco: ")),
-                 orientacion = str(input("Indica la orientación: N, S, E, O: "))
-                 ):
-        self.nombre = nombre
-        self.eslora = eslora
-        self.orientacion = orientacion
--- tablero.py
+++
@@ -1,5 +0,0 @@
-class Tablero:
-    def __init__(self, barcos, lado = 10):
-        self.lado = lado
-        self.barcos = barcos
-        self.tablero = np.zeros(lado, lado)
